cc92a2b9c260e55caa03bbd6fcd09e9f9b7dcc17
BOsmers <benjamin@convergenc3.com>
2022-11-10 09:15:59 +0200

    💄  Heading animations

--- src/components/SectionHeading.js
+++ src/components/SectionHeading.js
@@ -1,21 +1,26 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const SectionHeading = ({ title, padding, color }) => {
   return (
-    <div
+    <motion.div
       style={{
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
         paddingBottom: padding ? padding : 80,
       }}
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 1 }}
     >
       <div
         className="mx-3"
         style={{
           backgroundColor: "rgba(209, 0, 201, 1)",
           height: 5,
-          width: '5%'
+          width: "5%",
         }}
       ></div>
       <h2
@@ -25,7 +30,7 @@ const SectionHeading = ({ title, padding, color }) => {
       >
         {title}
       </h2>
-    </div>
+    </motion.div>
   );
 };
 
